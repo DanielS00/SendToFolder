dbdd32db068a32b51d2881d61b82d26701a93745
Daniel Saury <daniel.saury.00@gmail.com>
2019-10-03 13:18:13 +0300

    Typo

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ cls
 echo SendToFolder installation
 powershell -executionpolicy bypass -file ".\scripts\install.ps1"
 if exist %appdata%\Microsoft\Windows\SendTo\Folder.lnk (
-    echo "Installation successful!"
+    echo Installation successful!
 ) else (
     echo Something went wrong.
 )
